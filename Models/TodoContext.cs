fed5d0e7603486cf070e219588c10e1f93113f38
Abner Raymundo <abner.lucas@itspossiblefor.me>
2020-01-24 11:06:37 -0300

    Classe model da lista todo e contexto do DB

---
+++ Models/TodoContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace apicorenet.Models
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+        : base(options)
+        {
+
+        }
+
+        public DbSet<TodoItem> TodoItems {get; set;}
+
+    }
+}
